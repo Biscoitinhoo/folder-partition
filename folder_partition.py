b1353b573b9a2546643461802e08e8b8e287889d
Biscoitinhoo <gustavo.dickmann123@gmail.com>
2021-09-20 14:06:49 -0300

    creating val/train folders with all original content.

--- folder_partition.py
+++ folder_partition.py
@@ -29,7 +29,7 @@ def main():
 
 
 def usage():
-    print("""Split your data into 'training' and 'validation' folders. By default, it will take 80% of all your data inside each folder of the specified path and will split into the mentioned folders.""")
+    print("""Split your data into 'training' and 'validation' folders. By default, it will take 80% of all your data and put into the 'training' folder and the remaining into the 'validation' folder.""")
     print('You can change the quantity/percentage using the arguments.')
     print('')
 
@@ -45,8 +45,8 @@ def usage():
     print('Examples')
 
     print('')
-    print("Insert 80% of the data of the specified folder into 'training' folder and 20% into 'validation' folder.")
-    print('     python3 folder-partition.py -f /home/biscoitinho/mnist_dataset -q 80 -pe')
+    print("Insert 60% of the data of the specified folder into 'training' folder and 40% into 'validation' folder.")
+    print('     python3 folder-partition.py -f /home/biscoitinho/mnist_dataset -q 60 -pe')
     print('')
     print("Insert 800 of the data of the specified folder into 'training' folder and the rest into the 'validation' folder.")
     print('     python3 folder-partition.py --folder /home/biscoitinho/mnist_dataset --quantity 800')
@@ -61,14 +61,29 @@ def copy_files_to_folders(path):
 
     directories = os.listdir(path)
     for _dir in directories:
-        original_directories.append(path + '/' + _dir)
+        original_directories.append(_dir)
 
     create_training_and_validation_dir(path)
 
+    split_files_into_folders(original_directories, validation, path)
+    split_files_into_folders(original_directories, training, path)
+
+    print('Done.')
+
+
+def split_files_into_folders(original_directories, new_path, path):
     for d in original_directories:
-        print(d)
-        shutil.copytree(d, validation, dirs_exist_ok=True)
-        shutil.copytree(d, training, dirs_exist_ok=True)
+        # cd to training/validation folder
+        os.chdir(new_path)
+        # create original folders
+        os.makedirs(d, exist_ok=True)
+        # cd to created folder
+        os.chdir(d)
+
+        current_directory = os.path.abspath(os.getcwd())
+        root_dir = path + '/' + d
+        # copy files from root directory to the new one
+        shutil.copytree(root_dir, current_directory, dirs_exist_ok=True)
 
 
 def create_training_and_validation_dir(directory):
